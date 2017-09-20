23aba3119fe7e34ab4b7c31998d75c7f44e298a2
2016 iMac <2016imac@2016s-iMac.local>
2017-09-20 15:50:50 -0700

    upgrade to swift 4, add mic permissions to info.plist

--- Blowgun/GameScene.swift
+++ Blowgun/GameScene.swift
@@ -9,53 +9,73 @@
 import UIKit
 import SpriteKit
 
-class GameScene: SKScene {
-    override func didMoveToView(view: SKView)
+class GameScene: SKScene
+{
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let backdrop = SKSpriteNode(imageNamed:"backdrop")
+    let midground = SKSpriteNode(imageNamed:"midground")
+    let protag = SKSpriteNode(imageNamed:"protag_standing")
+    let blueBalloon = SKSpriteNode(imageNamed:"blueBalloon")
+    let redBalloon = SKSpriteNode(imageNamed:"redBalloon")
+    let yellowBalloon = SKSpriteNode(imageNamed:"yellowBalloon")
+    let projectileSprite = SKSpriteNode(imageNamed:"projectile1")
+    let foreground = SKSpriteNode(imageNamed:"foreground")
+
+    override func didMove(to view: SKView)
     {
-        let backdrop = SKSpriteNode(imageNamed:"backdrop")
         backdrop.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
         backdrop.xScale = 0.25
         backdrop.yScale = 0.25
         addChild(backdrop)
 
-        let midground = SKSpriteNode(imageNamed:"midground")
         midground.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
         midground.xScale = 0.25
         midground.yScale = 0.25
         addChild(midground)
 
-        let protag = SKSpriteNode(imageNamed:"protag_standing")
         protag.position = CGPoint(x: 150, y: 270)
         protag.xScale = 0.25
         protag.yScale = 0.25
         addChild(protag)
         
-        let blueBalloon = SKSpriteNode(imageNamed:"blueBalloon")
         blueBalloon.position = CGPoint(x: 800, y: 400)
         blueBalloon.xScale = 0.25
         blueBalloon.yScale = 0.25
         addChild(blueBalloon)
         
-        let redBalloon = SKSpriteNode(imageNamed:"redBalloon")
         redBalloon.position = CGPoint(x: 600, y: 500)
         redBalloon.xScale = 0.25
         redBalloon.yScale = 0.25
         addChild(redBalloon)
         
-        let yellowBalloon = SKSpriteNode(imageNamed:"yellowBalloon")
         yellowBalloon.position = CGPoint(x: 400, y: 300)
         yellowBalloon.xScale = 0.25
         yellowBalloon.yScale = 0.25
         addChild(yellowBalloon)
         
-        let foreground = SKSpriteNode(imageNamed:"foreground")
+        projectileSprite.position = CGPoint(x: 200, y: 300)
+        projectileSprite.xScale = 0.25
+        projectileSprite.yScale = 0.25
+        projectileSprite.isHidden = true
+        addChild(projectileSprite)
+        
         foreground.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
         foreground.xScale = 0.25
         foreground.yScale = 0.25
         addChild(foreground)
     }
     
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    override func update(_ currentTime: TimeInterval)
+    {
+        if appDelegate.firing
+        {
+            protag.texture = SKTexture(imageNamed: "protag_firing")
+            projectileSprite.isHidden =  false
+            //START ANIMATION WHERE ACCELEROMETER SETS ANGLE, appDelegate.projectileForce sets distance (or speed)
+        }
+        else
+        {
+            protag.texture = SKTexture(imageNamed: "protag_standing")
+        }
     }
 }
--- Blowgun/GameViewController.swift
+++ Blowgun/GameViewController.swift
@@ -8,40 +8,110 @@
 
 import UIKit
 import SpriteKit
-
+import AVFoundation
+import CoreAudio
 class GameViewController: UIViewController
 {
+    var recorder: AVAudioRecorder!
+    var levelTimer = Timer()
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
 
         // when the view controller appears rotate it to landscape mode
-        UIApplication.sharedApplication().setStatusBarOrientation(UIInterfaceOrientation.LandscapeRight, animated: true)
+        UIApplication.shared.setStatusBarOrientation(UIInterfaceOrientation.landscapeRight, animated: true)
 
         if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
             let skView = self.view as! SKView
             skView.showsFPS = true
             skView.showsNodeCount = true
             
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = false
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .Fill
-            
+            scene.scaleMode = .fill
             skView.presentScene(scene)
         }
+        
+        let session = AVAudioSession.sharedInstance()
+        do
+        {
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        }
+        catch let error as NSError
+        {
+            print("could not set session category")
+            print(error.localizedDescription)
+        }
+        do
+        {
+            try session.setActive(true)
+        }
+        catch let error as NSError
+        {
+            print("could not make session active")
+            print(error.localizedDescription)
+        }
+
+        let documents: AnyObject = NSSearchPathForDirectoriesInDomains( FileManager.SearchPathDirectory.documentDirectory,  FileManager.SearchPathDomainMask.userDomainMask, true)[0] as AnyObject
+        let str =  documents.appendingPathComponent("recordTest.caf") as String
+        let url = URL(fileURLWithPath: str as String)
+
+        let recordSettings:[String : AnyObject] = [
+            AVFormatIDKey: NSNumber(value: kAudioFormatAppleLossless as UInt32),
+            AVEncoderAudioQualityKey : AVAudioQuality.max.rawValue as AnyObject,
+            AVEncoderBitRateKey : 320000 as AnyObject,
+            AVNumberOfChannelsKey: 2 as AnyObject,
+            AVSampleRateKey : 44100.0 as AnyObject
+        ]
+
+        do
+        {
+            recorder = try AVAudioRecorder(url: url, settings: recordSettings)
+            recorder.delegate = self
+            recorder.isMeteringEnabled = true
+            recorder.prepareToRecord() // creates/overwrites the file at soundFileURL
+        }
+        catch let error as NSError
+        {
+            recorder = nil
+            print(error.localizedDescription)
+        }
+
+        recorder.record()            
+        self.levelTimer = Timer.scheduledTimer(timeInterval: 0.02, target: self, selector: #selector(GameViewController.levelTimerCallback), userInfo: nil, repeats: true)
     }
 
-    override func shouldAutorotate() -> Bool {
+    @objc func levelTimerCallback()
+    {
+        //we have to update meters before we can get the metering values
+        recorder.updateMeters()
+        
+        let force = recorder.averagePower(forChannel: 0)
+        if force > -15
+        {
+            if force > appDelegate.projectileForce
+            {
+                appDelegate.projectileForce = force
+            }
+            appDelegate.firing = true
+            /*print(recorder.averagePowerForChannel(0))
+            print("")*/
+        }
+        else
+        {
+            appDelegate.firing = false
+        }
+    }
+    
+    override var shouldAutorotate : Bool {
         return true
     }
 
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask
     {
-        return UIInterfaceOrientationMask.LandscapeRight
+        return UIInterfaceOrientationMask.landscapeRight
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,7 +119,38 @@ class GameViewController: UIViewController
         // Release any cached data, images, etc that aren't in use.
     }
 
-    override func prefersStatusBarHidden() -> Bool {
+    override var prefersStatusBarHidden : Bool {
         return true
     }
+}
+
+// MARK: AVAudioRecorderDelegate
+extension GameViewController : AVAudioRecorderDelegate {
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder,
+        successfully flag: Bool) {
+            print("finished recording \(flag)")
+            
+            // iOS8 and later
+            let alert = UIAlertController(title: "Recorder",
+                message: "Finished Recording",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Keep", style: .default, handler: {action in
+                print("keep was tapped")
+            }))
+            alert.addAction(UIAlertAction(title: "Delete", style: .default, handler: {action in
+                print("delete was tapped")
+                self.recorder.deleteRecording()
+            }))
+            self.present(alert, animated:true, completion:nil)
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder,
+        error: Error?) {
+            
+            if let e = error {
+                print("\(e.localizedDescription)")
+            }
+    }
+    
 }
